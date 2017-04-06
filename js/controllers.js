80b8f8060999cd709c10f840106bdb820de58072
rubenalmeida <ruben36almeida96@gmail.com>
2017-04-06 00:09:30 -0300

    listando do banco os pets cadastrados (ruben) 06/04

--- js/controllers.js
+++ js/controllers.js
@@ -1,57 +1,66 @@
 angular.module('app.controllers', [])
 
-  .controller('adoteCtrl', ['$scope', '$stateParams',
-    function ($scope, $stateParams) {
+    .controller('adoteCtrl', ['$scope', '$stateParams',
+        function ($scope, $stateParams) {
+
+            var db = firebase.database();
+            var ref = db.ref("user/1");
+
+            ref.on("value", function (snapshot) {
+
+                $scope.pets = snapshot.val();
+
+            }, function (errorObject) {
+                console.log("The read failed: " + errorObject.code);
+            });
+
+        }])
+
+    .controller('desaparecidosCtrl', ['$scope', '$stateParams',
+        function ($scope, $stateParams) {
 
 
-    }])
+        }])
 
-  .controller('desaparecidosCtrl', ['$scope', '$stateParams',
-    function ($scope, $stateParams) {
-
-
-    }])
-
-  .controller('perfilCtrl', ['$scope', '$stateParams',
-    function ($scope, $stateParams) {
+    .controller('perfilCtrl', ['$scope', '$stateParams',
+        function ($scope, $stateParams) {
 
 
-    }])
+        }])
 
-  .controller('menuCtrl', ['$scope', '$stateParams',
-    function ($scope, $stateParams) {
-
-
-    }])
-
-  .controller('loginCtrl', ['$scope', '$stateParams',
-    function ($scope, $stateParams) {
+    .controller('menuCtrl', ['$scope', '$stateParams',
+        function ($scope, $stateParams) {
 
 
-    }])
+        }])
 
-  .controller('filtrosCtrl', ['$scope', '$stateParams',
-
-    function ($scope, $stateParams) {
-
-
-    }])
-
-  .controller('addPetCtrl', ['$scope', '$stateParams',
+    .controller('loginCtrl', ['$scope', '$stateParams',
+        function ($scope, $stateParams) {
 
 
-    function ($scope, $stateParams) {
+        }])
+
+    .controller('filtrosCtrl', ['$scope', '$stateParams',
+
+        function ($scope, $stateParams) {
 
 
+        }])
 
-      $scope.pet = {};
+    .controller('addPetCtrl', ['$scope', '$stateParams',
 
-      $scope.addPet = function (pet) {
-        // $scope.pets.push(pet);
 
-        firebase.database().ref('user/1').push(pet)
+        function ($scope, $stateParams) {
 
-        $scope.pet = {}
-      }
 
-    }])
+            $scope.pet = {};
+
+            $scope.addPet = function (pet) {
+                // $scope.pets.push(pet);
+
+                firebase.database().ref('user/1').push(pet)
+
+                $scope.pet = {}
+            }
+
+        }])
