0dec60343b144b6b4ea64fe32799a1660dde2907
kurtye <kurtyebsb@gmail.com>
2017-04-07 09:40:00 -0300

    Add files via upload

---
+++ www/js/controllers.js
@@ -0,0 +1,65 @@
+angular.module('app.controllers', [])
+
+    .controller('adoteCtrl', ['$scope', '$stateParams',
+        function ($scope, $stateParams) {
+
+            var db = firebase.database();
+            var ref = db.ref("user/1");
+
+            ref.on("value", function (snapshot) {
+
+                $scope.pets = snapshot.val();
+
+            }, function (errorObject) {
+                console.log("Erro na leitura do banco " + errorObject.code);
+            });
+
+        }])
+
+    .controller('desaparecidosCtrl', ['$scope', '$stateParams',
+        function ($scope, $stateParams) {
+
+
+        }])
+
+    .controller('perfilCtrl', ['$scope', '$stateParams',
+        function ($scope, $stateParams) {
+
+
+        }])
+
+    .controller('menuCtrl', ['$scope', '$stateParams',
+        function ($scope, $stateParams) {
+
+
+        }])
+
+    .controller('loginCtrl', ['$scope', '$stateParams',
+        function ($scope, $stateParams) {
+
+
+        }])
+
+    .controller('filtrosCtrl', ['$scope', '$stateParams',
+
+        function ($scope, $stateParams) {
+
+
+        }])
+
+    .controller('addPetCtrl', ['$scope', '$stateParams',
+
+
+        function ($scope, $stateParams) {
+
+
+            $scope.pet = {};
+
+            $scope.addPet = function (pet) {
+
+                firebase.database().ref('user/1').push(pet)
+
+                $scope.pet = {}
+            }
+
+        }])
