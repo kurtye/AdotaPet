7d351f3e1062acdadffb1db060619ee82ff7e915
kurtye <kurtyebsb@gmail.com>
2017-04-10 00:42:35 -0300

    Versão 0.1.2
    - Login com Google Signin
    - Rotas Amigaveis (não completa)
    - Cadastro com email
    - Login com Email
    -  ng-model pet.especie
    - Função addPet
    - Conexão com firebase
    - Enviando Dados Para o DB Firebase
    
    - Versão estável

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -115,6 +115,35 @@ angular.module('app.controllers', [])
       };// fim $scope.doLogin()
 
 
+      // Login com Google
+
+      var provider = new firebase.auth.GoogleAuthProvider();
+
+      $scope.doLoginGoogle = function (userLoginGoogle) {
+
+        firebase.auth().signInWithPopup(provider).then(function (result) {
+          // This gives you a Google Access Token. You can use it to access the Google API.
+          var token = result.credential.accessToken;
+          // The signed-in user info.
+          var user = result.user;
+          // ...
+
+          $state.go("tabsController.adote");
+        }).catch(function (error) {
+          // Handle Errors here.
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          // The email of the user's account used.
+          var email = error.email;
+          // The firebase.auth.AuthCredential type that was used.
+          var credential = error.credential;
+          // ...
+
+        });
+
+      }//Fim do login com Google
+
+
     }])
 
   .controller('filtrosCtrl', ['$scope', '$stateParams',
