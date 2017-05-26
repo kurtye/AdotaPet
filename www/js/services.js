fc29527b85faee27b6d8ba41f9ca9e35749c9bdd
kurtye <kurtyebsb@gmail.com>
2017-05-26 02:11:43 -0300

    Versão 1.0 - Login Nativo (Facebook e G+) - Varias Modificações - Photo do Perfil - Session Storage - Perfil do pet - Listar Pets em adoção no menu Lateral - Login com Facebook - Login com Google Signin - Rotas Amigaveis (não completa) - Cadastro com email - Login com Email -  ng-model pet.especie - Função addPet - Conexão com firebase - Enviando Dados Para o DB Firebase
    - Versão estável

--- www/js/services.js
+++ www/js/services.js
@@ -1,18 +1,18 @@
 angular.module('app.services', [])
 
-.factory('BlankFactory', [function(){
+  .factory('BlankFactory', [function () {
 
-}])
+  }])
 
-  .service('UserService', function() {
+  .service('UserService', function () {
     // For the purpose of this example I will store user data on ionic local storage but you should save it on a database
-    var setUser = function(user_data) {
+    var setUser = function (user_data) {
       window.localStorage.starter_facebook_user = JSON.stringify(user_data);
 
       firebase.database().ref('usuarios/' + user.userId).set(user_data);
     };
 
-    var getUser = function(){
+    var getUser = function () {
       return JSON.parse(window.localStorage.starter_facebook_user || '{}');
     };
 
