d8b40335bc706402abd1b60a2219abbd83523eae
rubenalmeida <ruben36almeida96@gmail.com>
2017-06-13 15:40:53 -0300

    chatCtrl v1

--- www/js/controllers/chatCtrl.js
+++ www/js/controllers/chatCtrl.js
@@ -1,7 +1,19 @@
-angular.module('chatCtrls', []).controller('chatCtrl', ['$scope', '$rootScope', '$stateParams', 'ChatService',
-  function ($scope, $rootScope, $stateParams, ChatService) {
+angular.module('chatCtrls', []).controller('chatCtrl', ['$scope', '$rootScope', '$state', 'ChatService', 'UsuarioService', '$ionicModal',
+    function ($scope, $rootScope, $state, ChatService, UsuarioService, $ionicModal) {
 
-    $scope.chats = ChatService.getMyChats();
 
-    console.log($scope.chats);
-  }]);
+        var salasEnvios = ChatService.getSalasEnviadas();
+        var salasRecebidas = ChatService.getSalasRecebidas();
+        $scope.salasEnvios = salasEnvios;
+        $scope.salasRecebidas = salasRecebidas;
+
+
+        $scope.abrirChat = function (chat) {
+
+            // O padrão é um objeto, a key do pet, e os dados agrupados.
+            var retorno = ChatService.setDados(chat, chat.pet, chat.dono_interessado_pet);
+
+            $state.go('tabsController.conversa', {id: chat.dono_interessado_pet, dono: chat.id_dono})
+        }
+
+    }]);
