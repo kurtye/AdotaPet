8cafc207c52718a4589b1325be95358a9b6f89e3
rubenalmeida <ruben36almeida96@gmail.com>
2017-06-13 15:06:47 -0300

    petService setando os dados do pet em variaveis pra controler pegar os volores delas.

--- www/js/services/PetsService.js
+++ www/js/services/PetsService.js
@@ -3,20 +3,61 @@ angular.module('PetServices', [])
 
         const rootRef = firebase.database();
 
-        this.getPets = function (id) {
-
-            if (id) {
-
-                return rootRef.ref("adocao/pets/" + id);
+        var pet = {
+            descricao:     null,
+            especie:       null,
+            estado:        null,
+            idade:         null,
+            imgURL:        null,
+            nome:          null,
+            dt_publicacao: null,
+            user: {
+                email: null,
+                nome:  null,
+                foto:  null,
+                id:    null
             }
-            return rootRef.ref('adocao/pets/');
+        };
+
+        this.setPet = function (key) {
+
+            if (!key) {
+                return false;
+            }
+
+            rootRef.ref("adocao/pets/" + key).once("value", function (snap) {
+                pet.descricao = snap.val().descricao;
+                pet.especie = snap.val().especieSelecionado;
+                pet.estado = snap.val().estado;
+                pet.idade = snap.val().idade;
+                pet.imgURL = snap.val().imgURL;
+                pet.nome = snap.val().nome;
+                pet.dt_publicacao = snap.val().time;
+
+                pet.user.email = snap.val().email;
+                pet.user.nome = snap.val().nomeUsuario;
+                pet.user.foto = snap.val().userFoto;
+                pet.user.id = snap.val().usuario;
+
+            }, function (errorObject) {
+                console.log("Erro ao setar pet" + errorObject.code);
+            });
 
 
         };
 
-        this.setPet = function (pet, key) {
+        this.getPet = function () {
+            return pet;
+        };
+        this.getPetsRef = function () {
 
-            if (key){
+            return rootRef.ref('adocao/pets/');
+
+        };
+
+        this.updatePet = function (pet, key) {
+
+            if (key) {
 
                 return firebase.database().ref('adocao/pets/' + key).set(pet);
             }
