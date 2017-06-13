83e51db661b7d4bfc589d86bc1c0ba37c6c85898
rubenalmeida <ruben36almeida96@gmail.com>
2017-06-13 15:41:26 -0300

    adoteCtrl v1

--- www/js/controllers/adoteCtrl.js
+++ www/js/controllers/adoteCtrl.js
@@ -1,13 +1,14 @@
 angular.module('adoteCtrls', []).controller('adoteCtrl', ['$scope', '$stateParams', '$state', '$rootScope', 'PetService',
     function ($scope, $stateParams, $state, $rootScope, PetService) {
 
-        var pets = [];
+        var pets = {};
         $rootScope.pets = pets;
 
         var usuario = $rootScope.usuario;
 
-        PetService.getPets().on("value", function (snap) {
-            pets.push(snap.val());
+        PetService.getPetsRef().on("child_added", function (snap) {
+            pets[snap.key] = snap.val();
+
         }, function (errorObject) {
             console.log("Erro na leitura do banco " + errorObject.code);
         });
