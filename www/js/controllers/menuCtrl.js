91b5fd6b8f15b77a52749ce0f355876e7443d6b2
rubenalmeida <ruben36almeida96@gmail.com>
2017-06-16 22:48:09 -0300

    nova service alteracoes na rota e no form

--- www/js/controllers/menuCtrl.js
+++ www/js/controllers/menuCtrl.js
@@ -1,30 +1,11 @@
 angular.module('menuCtrls', [])
-    .controller('menuCtrl', ['$scope', '$stateParams', '$rootScope', 'UsuarioService', '$state', '$ionicModal',
-        function ($scope, $stateParams, $rootScope, UsuarioService, $state, $ionicModal) {
+    .controller('menuCtrl', ['$scope', '$stateParams', '$rootScope', 'UsuarioService', '$state',
+        function ($scope, $stateParams, $rootScope, UsuarioService, $state) {
 
-            $ionicModal.fromTemplateUrl('templates/modal.html', {
-                scope: $scope,
-                animation: 'slide-in-up'
-            }).then(function (modal) {
-                $rootScope.modal = modal;
-            });
 
-            $scope.carregarModal = function (pet, key) {
-                $rootScope.pet = pet;
-                $rootScope.key = key;
-                $scope.modal.show();
-            };
             $scope.usuario = UsuarioService.getUser() ? UsuarioService.getUser() : {"userId": 'default'};
 
-            var usuario = $scope.usuario;
 
-            const db = firebase.database().ref();
-
-            var myPets = db.child('adocao/pets').orderByChild('usuario').equalTo(usuario.userId);
-            myPets.on('value', function (snap) {
-                $scope.myPets = snap.val();
-                //console.log(snap.val());
-            });
 
             $scope.doLogout = function () {
                 localStorage.clear();
