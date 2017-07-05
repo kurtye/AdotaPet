2db01d9f3460681e2ee40fae5885aa1f28b1472a
rubenalmeida <rubensantosdealmeida@live.com>
2017-07-04 21:40:05 -0300

    Add files via upload

---
+++ www/js/controllers/conversaCtrl.js
@@ -0,0 +1,33 @@
+angular.module('conversaCtrls', []).controller('conversaCtrl', ['$scope', '$rootScope', '$state', 'UsuarioService', 'ChatService', '$ionicScrollDelegate', '$stateParams',
+    function ($scope, $rootScope, $state, UsuarioService, ChatService, $ionicScrollDelegate, $stateParams) {
+
+        $scope.msg = [];
+
+        var messages = [];
+        $scope.messages = messages;
+        $scope.myId = UsuarioService.getUser().userId;
+        var chaveChat = $stateParams.id;
+        var dono = $stateParams.dono;
+
+
+
+        ChatService.getMessages(chaveChat).on('child_added', function (snap) {
+            messages.push(snap.val());
+            $scope.$apply();
+            $ionicScrollDelegate.resize();
+            $ionicScrollDelegate.scrollBottom(true);
+        });
+
+
+        $scope.sendMessage = function (msg) {
+
+            var retorno = ChatService.enviarMensagem(msg);
+            $scope.msg = '';
+
+        };
+
+
+        $ionicScrollDelegate.resize();
+        $ionicScrollDelegate.scrollBottom(true);
+
+    }]);
