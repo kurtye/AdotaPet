c0c2fa2e40813b2002f16989b7899738bedbc43c
rubenalmeida <rubensantosdealmeida@live.com>
2017-07-04 22:34:47 -0300

    Update routes.js

--- www/js/routes.js
+++ www/js/routes.js
@@ -1,167 +1,150 @@
 angular.module('app.routes', ['ionicUIRouter'])
 
-    .config(function ($stateProvider, $urlRouterProvider) {
+  .config(function ($stateProvider, $urlRouterProvider) {
 
-        // Ionic uses AngularUI Router which uses the concept of states
-        // Learn more here: https://github.com/angular-ui/ui-router
-        // Set up the various states which the app can be in.
-        // Each state's controller can be found in controllers.js
+    $stateProvider
+      .state('tabs.chat', {
+        url: '/chat',
+        views: {
+          'tab5': {
+            templateUrl: 'templates/chat.html',
+            controller: 'chatCtrl'
+          }
+        }
+      })
 
-        /*
-         The IonicUIRouter.js UI-Router Modification is being used for this route.
-         To navigate to this route, do NOT use a URL. Instead use one of the following:
-         1) Using the ui-sref HTML attribute:
-         ui-sref='tabs.perfil'
-         2) Using $state.go programatically:
-         $state.go('tabs.perfil');
-         This allows your app to figure out which Tab to open this page in on the fly.
-         If you're setting a Tabs default page or modifying the .otherwise for your app and
-         must use a URL, use one of the following:
-         /page1/tab1/page3
-         /page1/tab4/page3
-         */
+      .state('tabs.conversa', {
+        url: '/conversa/:id',
+        views: {
+          'tab5': {
+            templateUrl: 'templates/conversa.html',
+            controller: 'conversaCtrl'
+          }
+        }
 
-        $stateProvider
-            .state('tabs.chat', {
-                url: '/chat',
-                views: {
-                    'tab5': {
-                        templateUrl: 'templates/chat.html',
-                        controller: 'chatCtrl'
-                    }
-                }
-            })
-
-            .state('tabs.conversa', {
-                url: '/conversa/:id',
-                views: {
-                    'tab5': {
-                        templateUrl: 'templates/conversa.html',
-                        controller: 'conversaCtrl'
-                    }
-                }
-
-            })
+      })
 
 
-            .state('tabs.adote', {
-                url: '/adote',
-                views: {
-                    'tab1': {
-                        templateUrl: 'templates/adote.html',
-                        controller: 'adoteCtrl'
-                    }
-                }
-            })
+      .state('tabs.adote', {
+        url: '/adote',
+        views: {
+          'tab1': {
+            templateUrl: 'templates/adote.html',
+            controller: 'adoteCtrl'
+          }
+        }
+      })
 
 
-            .state('tabs.favoritos', {
-                url: '/favoritos',
-                views: {
-                    'tab1': {
-                        templateUrl: 'templates/favoritos.html',
-                        controller: 'favoritosCtrl'
-                    }
-                }
-            })
+      .state('tabs.favoritos', {
+        url: '/favoritos',
+        views: {
+          'tab1': {
+            templateUrl: 'templates/favoritos.html',
+            controller: 'favoritosCtrl'
+          }
+        }
+      })
 
-            .state('tabs.meuspets', {
-                url: '/meuspets',
-                views: {
-                    'meusPets': {
-                        templateUrl: 'templates/meusPets.html',
-                        controller: 'meusPetsCtrl'
-                    }
-                }
-            })
-
-
-            .state('tabs.perfil/:id', {
-                url: '/perfil/:id',
-                views: {
-                    'tab1': {
-                        templateUrl: 'templates/perfil.html',
-                        controller: 'perfilCtrl'
-                    }
-                }
-            })
-
-            .state('tabs', {
-                url: '/tab',
-                templateUrl: 'templates/tabs.html',
-                controller: 'tabsCtrl',
-                abstract: true
-            })
-
-            .state('login', {
-                url: '/login',
-                templateUrl: 'templates/login.html',
-                controller: 'loginCtrl'
-            })
-
-            .state('tabs.addPet', {
-                url: '/addPet/:id',
-                views: {
-                    'tab4': {
-                        templateUrl: 'templates/modals/addPet.html',
-                        controller: 'addPetCtrl'
-                    }
-                },
-                params: {
-                    id: null
-                }
-            })
-
-            .state('tabs.addInsta', {
-                url: '/addInsta',
-                views: {
-                    'tab2': {
-                        templateUrl: 'templates/addInsta.html',
-                        controller: 'addInstaCtrl'
-                    }
-                }
-            })
-
-            .state('tabs.instaPet', {
-                url: '/instaPet',
-                views: {
-                    'tab2': {
-                        templateUrl: 'templates/instaPet.html',
-                        controller: 'instaPetCtrl'
-                    }
-                }
-            })
-
-            .state('signup', {
-                url: '/signup',
-                templateUrl: "templates/signup.html",
-                controller: "signupCtrl"
-            })
-
-          .state('sobre', {
-            url: '/sobre',
-            templateUrl: "templates/modals/sobre.html",
-            controller: "menuCtrl"
-          })
-          .state('filtros', {
-            url: '/filtros',
-            templateUrl: "templates/filtros.html",
-            controller: "menuCtrl"
-          })
-          .state('apoio', {
-            url: '/apoio',
-            templateUrl: "templates/modals/apoio.html",
-            controller: "menuCtrl"
-          })
-
-          .state('relatarBugs', {
-            url: '/relatarBugs',
-            templateUrl: "templates/modals/ideas.html",
-            controller: "menuCtrl"
-          })
+      .state('tabs.meuspets', {
+        url: '/meuspets',
+        views: {
+          'meusPets': {
+            templateUrl: 'templates/meusPets.html',
+            controller: 'meusPetsCtrl'
+          }
+        }
+      })
 
 
-        $urlRouterProvider.otherwise('/login')
+      .state('tabs.perfil/:id', {
+        url: '/perfil/:id',
+        views: {
+          'tab1': {
+            templateUrl: 'templates/perfil.html',
+            controller: 'perfilCtrl'
+          }
+        }
+      })
+
+      .state('tabs', {
+        url: '/tab',
+        templateUrl: 'templates/tabs.html',
+        controller: 'tabsCtrl',
+        abstract: true
+      })
+
+      .state('login', {
+        url: '/login',
+        templateUrl: 'templates/login.html',
+        controller: 'loginCtrl'
+      })
+
+      .state('tabs.addPet', {
+        url: '/addPet/:id',
+        views: {
+          'tab4': {
+            templateUrl: 'templates/modals/addPet.html',
+            controller: 'addPetCtrl'
+          }
+        },
+        params: {
+          id: null
+        }
+      })
+
+      .state('tabs.addInsta', {
+        url: '/addInsta',
+        views: {
+          'tab2': {
+            templateUrl: 'templates/addInsta.html',
+            controller: 'addInstaCtrl'
+          }
+        }
+      })
+
+      .state('tabs.instaPet', {
+        url: '/instaPet',
+        views: {
+          'tab2': {
+            templateUrl: 'templates/instaPet.html',
+            controller: 'instaPetCtrl'
+          }
+        }
+      })
+
+      .state('signup', {
+        url: '/signup',
+        templateUrl: "templates/signup.html",
+        controller: "signupCtrl"
+      })
+
+      .state('sobre', {
+        url: '/sobre',
+        templateUrl: "templates/modals/sobre.html",
+        controller: "menuCtrl"
+      })
+
+      .state('filtros', {
+        url: '/filtros',
+        templateUrl: "templates/filtros.html",
+        controller: "filtroCtrl"
+      })
+
+      .state('apoio', {
+        url: '/apoio',
+        templateUrl: "templates/modals/apoio.html",
+        controller: "menuCtrl"
+      })
+
+      .state('relatarBugs', {
+        url: '/relatarBugs',
+        templateUrl: "templates/modals/ideas.html",
+        controller: "menuCtrl"
+      });
 
 
-    });
+    $urlRouterProvider.otherwise('/login')
+
+
+  });
