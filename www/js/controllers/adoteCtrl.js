5c35bd1b1e5dfd7040f514f9eaeff3e01e74c381
rubenalmeida <rubensantosdealmeida@live.com>
2017-07-04 22:14:31 -0300

    filtros v1

--- www/js/controllers/adoteCtrl.js
+++ www/js/controllers/adoteCtrl.js
@@ -1,30 +1,50 @@
-angular.module('adoteCtrls', []).controller('adoteCtrl', ['$scope', '$stateParams', '$state', '$rootScope', 'PetService', '$ionicLoading',
-    function ($scope, $stateParams, $state, $rootScope, PetService, $ionicLoading) {
+angular.module('adoteCtrls', []).controller('adoteCtrl', ['$scope', '$stateParams', '$state', '$rootScope', 'PetService', '$ionicLoading', 'ApoioService',
+    function ($scope, $stateParams, $state, $rootScope, PetService, $ionicLoading, ApoioService) {
 
         $ionicLoading.show({
             template: '<ion-spinner icon="android" class="spinner-balanced"></ion-spinner>'
         });
-        $scope.pets = [];
+        var log = function (data1, data2) {
+            console.log(data1, data2);
+        };
+        var pets = [];
+        $scope.pets = pets;
+
+        var filtros = ApoioService.getFiltros();
+        $scope.filtros = filtros;
+
+        log(filtros);
         PetService.getPetsRef().on("child_added", function (snap) {
             var key = snap.key;
+            var val = snap.val();
             var obj = {"key": key, "val": snap.val()};
-            //console.log(tumb);
-            $scope.pets.unshift(obj);
+
+            if (filtros.sexo == "" ||  filtros.sexo == val.sexo) {
+                if (filtros.outros && val.especie == "Outros" || filtros.canina && val.especie == "Canina" || filtros.felina && val.especie == "Felina") {
+                    if (val.estado == filtros.estado) {
+                        pets.unshift(obj);
+                    }
+                }
+            }
             $ionicLoading.hide();
 
         });
 
         PetService.getPetsRef().on("child_removed", function (snap) {
 
-            console.log(snap.val(), 'adadadas');
+            pets.forEach(function (item, index) {
+               if (item.key == snap.key) {
+                   log(item, index)
+                   pets.splice(index, 1);
+                   $scope.$apply();
+               }
+            });
 
         });
-        $scope.apply = function () {
-            $scope.$apply();
-        };
 
         $scope.detalharPet = function (id) {
             $state.go('tabs.perfil/:id', {id: id});
 
         };
+
     }]);
