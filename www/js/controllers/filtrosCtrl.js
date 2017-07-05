7215b7123db4dd114033adc07bb849ea3adc0d74
rubenalmeida <rubensantosdealmeida@live.com>
2017-07-04 22:30:11 -0300

    filtros v1

--- www/js/controllers/filtrosCtrl.js
+++ www/js/controllers/filtrosCtrl.js
@@ -1,7 +1,22 @@
-angular.module('filtrosCtrls', []).controller('filtrosCtrl', ['$scope', '$rootScope', '$stateParams',
-    function ($scope, $rootScope, $stateParams) {
+angular.module('filtrosCtrls', []).controller('filtroCtrl', ['$scope', '$rootScope', '$stateParams', '$state', 'ApoioService',
+    function ($scope, $rootScope, $stateParams, $state, ApoioService) {
 
-     
+        $scope.filtro = ApoioService.getFiltros();
 
+        $scope.filtrar = function (filtro) {
+            var filtros = ApoioService.setFiltros(filtro);
+            swal({
+                    title: 'Salvo',
+                    text: "Filtros aplicados",
+                    type: 'success',
+                    showConfirmButton: false,
+                    timer: 1000
+                }
+            );
+        };
 
+        $scope.fechar = function () {
+            $state.go('tabs.adote');
+            window.location.reload();
+        };
     }]);
