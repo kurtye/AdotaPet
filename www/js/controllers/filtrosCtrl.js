70733cc2b23a0de97d063b43f954bb422715d2ec
rubenalmeida <ruben36almeida96@gmail.com>
2017-07-19 19:14:28 -0300

    filtros recarregando a pagina sem bug

--- www/js/controllers/filtrosCtrl.js
+++ www/js/controllers/filtrosCtrl.js
@@ -1,26 +1,25 @@
 angular.module('filtrosCtrls', []).controller('filtroCtrl', ['$scope', '$rootScope', '$stateParams', '$state', 'ApoioService',
-    function ($scope, $rootScope, $stateParams, $state, ApoioService) {
+  function ($scope, $rootScope, $stateParams, $state, ApoioService) {
 
-        $scope.filtro = ApoioService.getFiltros();
+    $scope.filtro = ApoioService.getFiltros();
 
-        $scope.filtrar = function (filtro) {
-            var filtros = ApoioService.setFiltros(filtro);
-            swal({
-                    title: 'Salvo',
-                    text: "Filtros aplicados",
-                    type: 'success',
-                    showConfirmButton: false,
-                    timer: 1000
-                }
-            );
-          $state.go('tabs.adote');
-          window.location.reload();
-        };
+    $scope.filtrar = function (filtro) {
+      var filtros = ApoioService.setFiltros(filtro);
+      swal({
+          title: 'Salvo',
+          text: "Filtros aplicados",
+          type: 'success',
+          showConfirmButton: false,
+          timer: 1000
+        }
+      );
+      $state.go('tabs.adote', {}, {reload: true});
+    };
 
-        $scope.fechar = function () {
-            $state.go('tabs.adote');
-            window.location.reload();
-        };
+    $scope.fechar = function () {
+      $state.go('tabs.adote', {}, {reload: true});
+
+    };
 
 
-    }]);
+  }]);
