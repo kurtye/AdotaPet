7682ed9d9d78f7fda2d9795d84f921a2f508d136
kurtye <kurtyebsb@gmail.com>
2017-09-12 16:20:27 -0300

    Identação

--- www/js/controllers/addPetCtrl.js
+++ www/js/controllers/addPetCtrl.js
@@ -1,93 +1,97 @@
 angular.module('addPetCtrls', []).controller('addPetCtrl', ['$scope', '$stateParams', '$state', '$rootScope', 'UsuarioService', 'PetService', 'ApoioService',
-    function ($scope, $stateParams, $state, $rootScope, UsuarioService, PetService, ApoioService) {
+  function ($scope, $stateParams, $state, $rootScope, UsuarioService, PetService, ApoioService) {
 
-        $scope.key = $stateParams.id;
-        var key = $scope.key;
-        $scope.pet = {};
-        if(key){
-            PetService.setPet(key);
-            $scope.pet = PetService.getPet();
-        }
-        $scope.imgURL = document.getElementById("files");
-        //INICIO DO UPLOAD
-        window.previewFile = function previewFile() {
-            var storage = firebase.storage();
+    $scope.key = $stateParams.id;
+    var key = $scope.key;
+    $scope.pet = {};
+    if (key) {
+      PetService.setPet(key);
+      $scope.pet = PetService.getPet();
+    }
+    $scope.imgURL = document.getElementById("files");
+    //INICIO DO UPLOAD
+    window.previewFile = function previewFile() {
+      var storage = firebase.storage();
 
-            var file = document.getElementById("files").files[0];
-            console.log(file);
+      var file = document.getElementById("files").files[0];
+      console.log(file);
 
-            var storageRef = firebase.storage().ref();
+      var storageRef = firebase.storage().ref();
 
-            //dynamically set reference to the file name
-            if (key) {
-              var thisRef = storageRef.child('images/adocao/' + key + '/' + file.name);
-            }else {
-              var petKey = PetService.getNewPetKey().key;
-              $scope.key = petKey;
-              console.log(petKey);
+      //dynamically set reference to the file name
+      if (key) {
+        var thisRef = storageRef.child('images/adocao/' + key + '/' + file.name);
+        console.log(thisRef);
 
-              var thisRef = storageRef.child('images/adocao/' + petKey + '/' + file.name);
+      } else {
+        var petKey = PetService.getNewPetKey().key;
+        $scope.key = petKey;
 
-            }
+        var thisRef = storageRef.child('images/adocao/' + petKey + '/' + file.name);
 
-
-            //put request upload file to firebase storage
-            thisRef.put(file).then(function (snapshot) {
-                var url = snapshot.downloadURL;
+        firebase.database().ref().child('adocao/pets/' + petKey).set({default: '0'})
 
 
-                document.getElementById('linkbox').innerHTML = '<img src="' + url + '" style=" width: 100px; " />';
+      }
 
 
-                $scope.pet.imgURL = url;
-                console.log(url);
-            });
-
-            //get request to get URL for uploaded file
-            thisRef.getDownloadURL().then(function (url) {
-
-                console.log(url);
-            })
-
-        };
+      //put request upload file to firebase storage
+      thisRef.put(file).then(function (snapshot) {
+        var url = snapshot.downloadURL;
 
 
-        var user = UsuarioService.getUser();
-        $scope.pet.user = {
-            "id": user.userId,
-            "nome": user.displayName,
-            "email": user.email,
-            "foto": user.imageUrl
-        };
-
-        $scope.addPet = function (pet) {
-
-            //CHAMANDO O METODO DA SERVICE PASSANDO O OBJETO DE PET PARA INSERIR NO BANCO.
-            //SE TIVER A KEY A SERVICE VAI ALTERAR, SE NÃO ELA VAI INSERIR;
-            console.log(pet);
-
-            PetService.updatePet(pet, $scope.key);
-
-            $state.go("tabs.meuspets");
-
-            swal({
-                title: pet.nome + " foi colocado em adoção",
-                text: "Boa Sorte",
-                type: "success",
-                showConfirmlButton: false,
-                timer: 2000
-            });
-
-            $scope.pet = {};
-        };
+        document.getElementById('linkbox').innerHTML = '<img src="' + url + '" style=" width: 100px; " />';
 
 
-        $scope.especies = ApoioService.especies;
-        $scope.racas = ApoioService.getRacas($rootScope.especie);
+        $scope.pet.imgURL = url;
+        console.log(url);
+      });
 
-        $scope.GetSelectedEspecie = function (selecionada) {
-            console.log(selecionada);
-            $scope.racas = ApoioService.getRacas(selecionada);
-        };
+      //get request to get URL for uploaded file
+      thisRef.getDownloadURL().then(function (url) {
 
-    }]);
+        console.log(url);
+      })
+
+    };
+
+
+    var user = UsuarioService.getUser();
+    $scope.pet.user = {
+      "id": user.userId,
+      "nome": user.displayName,
+      "email": user.email,
+      "foto": user.imageUrl
+    };
+
+    $scope.addPet = function (pet) {
+
+      //CHAMANDO O METODO DA SERVICE PASSANDO O OBJETO DE PET PARA INSERIR NO BANCO.
+      //SE TIVER A KEY A SERVICE VAI ALTERAR, SE NÃO ELA VAI INSERIR;
+      console.log(pet);
+
+      PetService.updatePet(pet, $scope.key);
+
+      $state.go("tabs.meuspets");
+
+      swal({
+        title: pet.nome + " foi colocado em adoção",
+        text: "Boa Sorte",
+        type: "success",
+        showConfirmlButton: false,
+        timer: 2000
+      });
+
+      $scope.pet = {};
+    };
+
+
+    $scope.especies = ApoioService.especies;
+    $scope.racas = ApoioService.getRacas($rootScope.especie);
+
+    $scope.GetSelectedEspecie = function (selecionada) {
+      console.log(selecionada);
+      $scope.racas = ApoioService.getRacas(selecionada);
+    };
+
+  }]);
